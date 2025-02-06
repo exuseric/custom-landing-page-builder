17d68f84a657aa30070b30afaa9227248ec84f30
exuseric <ericbro29@gmail.com>
2025-02-06 14:19:41 +0300

    Refactor pocketbase.ts to integrate type definitions for PageType and ObjectType, update pageId, and enhance data retrieval with final URL construction.

--- src/utils/pocketbase.ts
+++ src/utils/pocketbase.ts
@@ -1,45 +1,11 @@
 import PocketBase from 'pocketbase';
-
-interface LandingPage {
-    title: string;
-    url: string;
-    meta_title: string;
-    meta_description: string;
-    theme_color: string;
-    country: string;
-    analytics_id: string;
-    search_console: string;
-    links: [string];
-    nav_style: string;
-    hero_title: string;
-    hero_excerpt: string;
-    hero_style: string;
-    contact_title: string;
-    contact_style: string;
-    core_values: string;
-    footer_style: string;
-    expand: {
-        logo: {
-            [key: string]: string;
-        };
-        hero_image: {
-            [key: string]: string;
-        };
-        sections: {
-            [key: string]: string;
-        };
-        contact_info: {
-            [key: string]: string;
-        };
-        contact_location: {
-            [key: string]: string;
-        };
-    }
-}
+import type { ObjectType, PageType } from "@utils/types"
+import { usePageData } from '@src/store/store';
 const pb = new PocketBase('http://127.0.0.1:8090');
-const pageId = 'sdv06887x54om9i'
+const pageId = 'd1yu92ds35z466b'
+const {setPageData} = usePageData()
 
-const parentUrls: Record<string, string> = {
+const parentUrls: ObjectType = {
     ke: 'https://www.yellowpageskenya.com/',
     cv: 'https://www.paginasamarelas.cv/',
     mz: 'https://www.paginasamarelas.co.mz/',
@@ -47,7 +13,15 @@ const parentUrls: Record<string, string> = {
     st: 'https://www.paginasamarelas.st/',
 };
 
-const parentSites: Record<string, string> = {
+const parentDomains: ObjectType = {
+    ke: 'yellowpageskenya.com',
+    cv: 'paginasamarelas.cv',
+    mz: 'paginasamarelas.co.mz',
+    tz: 'yellow.co.tz',
+    st: 'paginasamarelas.st',
+}
+
+const parentSites: ObjectType = {
     ke: 'Yellow Pages Kenya',
     cv: 'Páginas Amarelas de Cabo Verde',
     mz: 'Páginas Amarelas Moçambique',
@@ -60,13 +34,16 @@ const getImageUrl = ({ collection, filename }: { collection: any, filename: stri
 
 const getData = async () => {
     await pb.collection("_superusers").authWithPassword('eric.gathoni@yellowpageskenya.com', 'CDz5pFLmm3thaFZ');
-    const data = await pb.collection("landing_page").getOne<LandingPage>(pageId, {expand: 'logo,hero_image,sections, sections.image, sections.section_grid, sections.section_grid.image, contact_info, contact_location'})
+    const data = await pb.collection("landing_page").getOne<PageType>(pageId, {expand: 'logo,hero_image,hero_grid,sections, sections.image, sections.section_grid, sections.section_grid.image, contact_info, contact_location'})
     
     const company = {
         url: parentUrls[data.country],
         name: parentSites[data.country],
     }
-    const record = { ...data, company }
+    const finalUrl = `https://${data.title.split(' ').join('').toLocaleLowerCase()}.${parentDomains[data.country]}`
+    console.log(finalUrl)
+    const record = { ...data, company, finalUrl }
+    setPageData(record)
     return record
 }
 
