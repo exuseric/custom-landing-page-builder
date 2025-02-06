4254cd75af0ae0a0375a44e51f5fac00d1d39876
exuseric <ericbro29@gmail.com>
2025-02-06 14:19:31 +0300

    Add type definitions for PageType and ObjectType in utils/types.ts

---
+++ src/utils/types.ts
@@ -0,0 +1,43 @@
+export type PageType = {
+    title: string;
+    url: string;
+    meta_title: string;
+    meta_description: string;
+    theme_color: string;
+    country: string;
+    analytics_id: string;
+    search_console: string;
+    links: [string];
+    nav_style: string;
+    hero_title: string;
+    hero_excerpt: string;
+    hero_style: string;
+    contact_title: string;
+    contact_style: string;
+    core_values: string;
+    footer_style: string;
+    expand: {
+        logo: {
+            [key: string]: string;
+        };
+        hero_image: {
+            [key: string]: string;
+        };
+        sections: {
+            [key: string]: string;
+        };
+        contact_info: {
+            [key: string]: string;
+        };
+        contact_location: {
+            [key: string]: string;
+        };
+        hero_grid: {
+            [key: string]: string;
+        }
+    }
+}
+
+export type ObjectType = {
+    [key: string]: string;
+}
