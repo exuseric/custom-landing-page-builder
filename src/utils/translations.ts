afc94f003fb72b9e268b34120a44b678bb7acf8c
exuseric <ericbro29@gmail.com>
2025-06-09 15:58:38 +0300

    feat: Implement Head component for SEO and font management; refactor PageNavigation and index pages to utilize new Head component; add font utility for consistent font usage across the application

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,5 +1,11 @@
 import navigationTranslations from './lang/navigation.json';
-
+export const languages: Record<string, string> = {
+	ke: "en-KE",
+	cv: "pt-CV",
+	mz: "pt-MZ",
+	tz: "en-TZ",
+	st: "pt-ST",
+};
 export function getLanguageFromCountry(country: string): 'en' | 'pt' {
     const ptCountries = ['cv', 'mz', 'st'];
     return ptCountries.includes(country) ? 'pt' : 'en';
