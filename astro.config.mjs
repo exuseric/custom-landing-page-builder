3f1fc20455572ea986d8205bd1ca7b6c0826f301
exuseric <ericbro29@gmail.com>
2025-06-11 10:24:54 +0300

    feat: Update image handling with @unpic/astro; refactor CustomImage component to simplify size logic and integrate blurhash placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,30 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
-import { getData } from './src/utils/pocketbase';
+import { imageService } from "@unpic/astro/service";
+
 import { fetchPage } from './src/utils/payload';
-const {url} = await fetchPage(1, ['url'])
+const { url } = await fetchPage(1, ['url'])
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   outDir: process.cwd() + '\\build\\' + url.slice(8),
   site: url,
-  experimental: {
-    responsiveImages: true
-  },
+  // experimental: {
+  //   responsiveImages: true
+  // },
   build: {
     assets: 'assets',
     inlineStylesheets: 'never',
-    
+
   },
   image: {
     domains: ["127.0.0.1"],
-    experimentalLayout: 'constrained',
+    // experimentalLayout: 'constrained',
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+
   },
   compressHTML: false,
   integrations: [sitemap(), icon()]
